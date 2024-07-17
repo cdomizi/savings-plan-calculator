9545ec1a66cf96c444cd77ab61fc5202eedf0579
cdomizi <domizicesare@gmail.com>
2024-07-17 10:17:23 +0200

    remove unnecessary code

--- modules/calculator.js
+++ modules/calculator.js
@@ -22,42 +22,7 @@ const getMonthsToGoalBalance = ({
   return months;
 };
 
-// Get the remmaining term to goal balance in years, months
-const getRemainingTerm = (monthsNumber) => {
-  const yearsCount = Math.floor(monthsNumber / 12);
-  const years = yearsCount > 1 ? "anni" : "anno";
-  const formatYears = yearsCount ? `${yearsCount} ${years}` : "";
-
-  const monthsCount = monthsNumber % 12;
-  const months = monthsCount > 1 ? "mesi" : "mese";
-  const formatMonths = monthsCount ? `${monthsCount} ${months}` : "";
-
-  const separator = formatYears && formatMonths && ", ";
-  const remainingTerm = `${formatYears}${separator}${formatMonths}`;
-
-  return remainingTerm;
-};
-
-// Get balance at any given date
-const getBalanceAtDate = (monthlyContribution, year, month) => {
-  let currentBalance = 0;
-  const currentDate = new Date();
-  const date = new Date();
-
-  date.setYear(year);
-  date.setMonth(month);
-
-  while (currentDate < date) {
-    currentBalance += monthlyContribution;
-    currentDate.setMonth(currentDate.getMonth() + 1);
-  }
-
-  return currentBalance;
-};
-
 export {
   addMonthsToDate,
   getMonthsToGoalBalance,
-  getRemainingTerm,
-  getBalanceAtDate,
 };
